2e1de59845064a68aa213b58622e207a517dcad9
coltonm <coltonmarshall@gmail.com>
2019-09-09 09:27:14 -0700

    Added InventoryCard.cs and start of Moleculicon

---
+++ 1909B_AlchemySimTwo_Unity_2018Legacy/Assets/InventoryCard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryCard : MonoBehaviour
+{
+    Solution solution;
+    public GameObject textObject;
+
+    private void Start()
+    {
+        solution = this.GetComponent<Solution>();
+        SetText();
+    }
+
+    void SetText()
+    {
+        print("INVENTORY CARD: solution stuff is " + solution.solutionMolecules);
+
+        TextMeshProUGUI text = textObject.GetComponent<TextMeshProUGUI>();
+        foreach (KeyValuePair<string, Molecule> entry in solution.solutionMolecules)
+        {
+            text.text += entry.Value.GetName();
+        }
+    }
+
+    public void SendSolution(GameObject gob)
+    {
+        Solution targetSolution = gob.GetComponent<Solution>();
+        foreach (KeyValuePair<string, Molecule> entry in solution.solutionMolecules)
+        {
+            targetSolution.solutionMolecules.Add(entry.Key, entry.Value);
+        }
+        print("TARGET SOLUTION SAYS: " + targetSolution.solutionMolecules);
+    }
+
+}
---
+++ 1909B_AlchemySimTwo_Unity_2018Legacy/Assets/Scripts/MoleculeTable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleculeTable : MonoBehaviour
+{
+    Dictionary<string, Molecule> moleculicon = new Dictionary<string, Molecule>();
+
+    public void SetMol(string name, Molecule mol)
+    {
+        moleculicon.Add(name, mol);
+    }
+
+    public Molecule GetMol (string name)
+    {
+        return moleculicon[name];
+    }
+}
